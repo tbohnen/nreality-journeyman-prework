9db40d75f62f67860e2f8726322c081f8e44faeb
Theo Bohnen <tbohnen.jnr@gmail.com>
2014-02-06 21:46:06 +0200

    started supermarket

---
+++ Supermarket/Supermarket/Models/StockItem.cs
@@ -0,0 +1,34 @@
+﻿namespace Supermarket.Models
+{
+    public class StockItem
+    {
+        private decimal _costPrice;
+        private string _stockCode;
+        private string _stockDescription;
+
+        public StockItem(string stockCode, string stockDescription, decimal costPrice)
+        {
+            _stockCode = stockCode;
+            _stockDescription = stockDescription;
+            _costPrice = costPrice;
+        }
+
+        public decimal CostPrice
+        {
+            get { return _costPrice; }
+            set { _costPrice = value; }
+        }
+
+        public string StockCode
+        {
+            get { return _stockCode; }
+            set { _stockCode = value; }
+        }
+
+        public string StockDescription
+        {
+            get { return _stockDescription; }
+            set { _stockDescription = value; }
+        }
+    }
+}
